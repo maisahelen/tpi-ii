d461d17d03202711ddbf9bc4015ee5d65e7c0e6c
maisa <maisa991485084@hmail.com>
2018-08-10 22:42:50 -0300

    helen fernanda

---
+++ aula1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2> Primeira Página PHP </h2>
+    <?php
+      print "<h1> Hello World (Dashlane)</h1>";
+      print "<h1> Hoje é sexta-feira <br/>". " Dia 03/08/2018 <br/> ". " A noite está chuvosa <br/>";
+      $nome;
+      $nome= "Maísa Helen";
+      print "Meu nome é $nome <br/>";
+      $x=1;
+      $y=2;
+      print "$x+$y=".($x+$y);
+      
+
+
+    ?>
+</body>
+</html>
